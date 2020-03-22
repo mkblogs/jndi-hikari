ea62fd502242101df319f3036db48598d559401f
Maria Kumar <tech.mkblog@gmail.com>
2020-03-22 23:30:03 +0530

    Initial commit

---
+++ src/main/java/com/tech/mkblogs/JndiHikariApplication.java
@@ -0,0 +1,13 @@
+package com.tech.mkblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JndiHikariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JndiHikariApplication.class, args);
+	}
+
+}
